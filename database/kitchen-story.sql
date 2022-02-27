facba1c40e21c50168d09bfa37006f8262a60987
Samruddhi Randive <84631708+samruddhi0507@users.noreply.github.com>
2022-02-27 14:51:46 +0530

    Changes have been Committed

--- database/kitchen-story.sql
+++ database/kitchen-story.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3307
--- Generation Time: Jul 14, 2021 at 11:38 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
